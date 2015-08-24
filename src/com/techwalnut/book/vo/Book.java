06fe069e4e28918e2d88d722a2652bd959e5d922
swapnil.domde <swapnildomde@gmail.com>
2015-08-24 18:53:54 +0530

    tostring  written

--- src/com/techwalnut/book/vo/Book.java
+++ src/com/techwalnut/book/vo/Book.java
@@ -3,5 +3,24 @@ package com.techwalnut.book.vo;
 public class Book {
 //	git remote add origin https://github.com/swapnildomde/BookApplication.git
 //		git push -u origin master
+	
+	
+	private String bookName;
+	private String id;
+	
+	
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+		bookName="c lang ";
+		id="101";
+	}
+
+
+	@Override
+	public String toString() {
+		return "Book [bookName=" + bookName + ", id=" + id + "]";
+	}
+	
 
 }
